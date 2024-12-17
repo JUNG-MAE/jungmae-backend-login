729bb8cae2f7856b5d3e49905798e4cc3c299528
Kim IkHwan <dlrghks2090@gmail.com>
2024-12-18 03:46:32 +0900

    Feat: 환경변수 설정 및 auth accessToken을 통한 로그인 및 회원가입으로 로직 수정

--- src/main/java/login/jungmae/login/config/jwt/JwtProperties.java
+++ src/main/java/login/jungmae/login/config/jwt/JwtProperties.java
@@ -3,7 +3,7 @@ package login.jungmae.login.config.jwt;
 public interface JwtProperties {
 
     String SECRET = "kan";
-    int ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60;   // 1분
+    int ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 5;   // 5분
     int REFRESH_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7;    // 7일
 
     String TOKEN_PREFIX = "Bearer ";
--- src/main/java/login/jungmae/login/controller/UserController.java
+++ src/main/java/login/jungmae/login/controller/UserController.java
@@ -28,8 +28,29 @@ public class UserController {
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
-    @PostMapping("/oauth2/token")
-    public ResponseEntity<?> loginAndGetToken(@RequestParam String code) {
+//    // 안드로이드 code access 불가로 인한 주석처리
+//    // code기반 로그인 구현 → accessKey기반 로그인 구현으로 수정
+//    @PostMapping("/oauth2/token")
+//    public ResponseEntity<?> loginAndGetToken(@RequestParam String code) {
+//
+//        System.out.println("====컨트롤러의 loginAndGetToken 메서드 입장====");
+//        System.out.println("code = " + code);
+//
+//        try {
+//            NaverTokenBody naverTokenBody = userService.getAccessToken(code);
+//            System.out.println("naverTokenBody = " + naverTokenBody);
+//            TokenDto tokenDto = userService.saveAndGetToken(naverTokenBody.getAccess_token());
+//
+//            System.out.println("    로그인 성공!    ");
+//            return new ResponseEntity<>(tokenDto, HttpStatus.OK);
+//        } catch (Exception e) {
+//            System.out.println("    로그인 실패!!!    ");
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+
+    @PostMapping("/oauth2/token/test")
+    public ResponseEntity<?> getAuthAccessToken(@RequestParam String code) {
 
         System.out.println("====컨트롤러의 loginAndGetToken 메서드 입장====");
         System.out.println("code = " + code);
@@ -38,15 +59,36 @@ public class UserController {
             NaverTokenBody naverTokenBody = userService.getAccessToken(code);
             System.out.println("naverTokenBody = " + naverTokenBody);
 
-            //String jwtToken =
-            TokenDto tokenDto = userService.saveAndGetToken(naverTokenBody.getAccess_token());
-
-            System.out.println("====!컨트롤러의 loginAndGetToken 메서드 퇴장!====");
-            return new ResponseEntity<>(tokenDto, HttpStatus.OK);
+            System.out.println("    auth AccessToken 발급 성공!    ");
+            return new ResponseEntity<>(naverTokenBody.getAccess_token(), HttpStatus.OK);
         } catch (Exception e) {
+            System.out.println("    auth AccessToken 발급 실패!!!    ");
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
+
+    @PostMapping("/oauth2/token")
+    public ResponseEntity<?> loginAndGetToken(@RequestParam String accessToken) {
+
+        System.out.println("====컨트롤러의 loginAndGetToken 메서드 입장====");
+        System.out.println("caccessToken = " + accessToken);
+
+        try {
+            // 안드로이드 code access 불가로 인한 주석처리
+            // code기반 로그인 구현 → accessKey기반 로그인 구현으로 수정
+//            NaverTokenBody naverTokenBody = userService.getAccessToken(code);
+//            System.out.println("naverTokenBody = " + naverTokenBody);
+//            TokenDto tokenDto = userService.saveAndGetToken(naverTokenBody.getAccess_token());
+            TokenDto tokenDto = userService.saveAndGetToken(accessToken);
+
+            System.out.println("    로그인 성공!    ");
+            return new ResponseEntity<>(tokenDto, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println("    로그인 실패!!!    ");
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
     @GetMapping("/oauth2/user")
     public ResponseEntity<?> getUser(HttpServletRequest request) {
@@ -64,7 +106,7 @@ public class UserController {
         } catch (NullPointerException e) {
             // 엑세스 토큰이 만료되었으므로 유효하지 않은 요청으므로 401 상태를 반환
             System.out.println("401에러 반환");
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>("accessToken expired", HttpStatus.UNAUTHORIZED);
         }
     }
 
